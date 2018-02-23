95a3cddbc6da370734a5ad2e3abca2459783e73c
Brenda Kiptim <kiptim54@gmail.com>
2018-02-23 17:38:39 +0300

    add html words

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 $("#choice").click(function(){
 var answer=parseInt($("#number").val());
 var calculate=divisible(answer);
+console.log(answer);
 
 });
 });
 //business Logic
+
 var divisible=function(answer){
   for(i=1; i<=answer; i++){
     if(i%3===0){
