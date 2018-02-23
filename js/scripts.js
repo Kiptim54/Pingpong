609d56aa8252b7decf9e0847c308561a5ee06309
Brenda Kiptim <kiptim54@gmail.com>
2018-02-23 11:31:11 +0300

    add draft user interface in js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+$("#choice").click(function(){
+var answer=parseInt($("#number").val());
+var divisibility=divisible(answer);
+$("#list").text(divisibilty);
+});
+});
+
+
+function
+if(answer%3===0){
+  alert(divisible);
+}else{
+  alert("not divisible");
+}
