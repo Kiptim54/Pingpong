ae9aa0101519a1d3e9c383976ab406326f5d0330
Brenda Kiptim <kiptim54@gmail.com>
2018-02-23 14:32:32 +0300

    setup javascript ping & pong

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@ $(document).ready(function(){
 $("#choice").click(function(){
 var answer=parseInt($("#number").val());
 var divisibility=divisible(answer);
-$("#list").text(divisibilty);
+$("#list").text(divisibility);
 });
 });
 
-
-function
-if(answer%3===0){
-  alert(divisible);
-}else{
-  alert("not divisible");
-}
+var divisible=function(answer){
+  for(i=1; i<=answer; i++){
+    if(i%3===0){
+      console.log("ping");
+    }else if (i%5===0){
+      console.log("pong");
+    } else if(i%15===0){
+      console.log("pingpong");
+    }else{
+      console.log(i);
+    }
+    }
+  }
