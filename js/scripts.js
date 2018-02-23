4c54f09db955631c0caa65a38a44953603acd710
Brenda Kiptim <kiptim54@gmail.com>
2018-02-23 16:23:54 +0300

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
+//User-Interface Logic
 $(document).ready(function(){
 $("#choice").click(function(){
 var answer=parseInt($("#number").val());
-var divisibility=divisible(answer);
+var calculate=divisible(answer);
 
 });
 });
-
+//business Logic
 var divisible=function(answer){
   for(i=1; i<=answer; i++){
     if(i%3===0){
-      $("#list").innerHTML="ping"
+      console.log("ping");
     }else if (i%5===0){
       console.log("pong");
     } else if(i%15===0){
