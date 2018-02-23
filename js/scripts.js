4addefc5e52df1a2d8e59073d9436b497c42d39f
Brenda Kiptim <kiptim54@gmail.com>
2018-02-23 20:15:39 +0300

    divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
 //User-Interface Logic
 $(document).ready(function(){
-$("#choice").click(function(){
+$("#choice").click(function(event){
 var answer=parseInt($("#number").val());
-var calculate=divisible(answer);
-console.log(answer);
+var resultf=divisible(answer);
+$("#output").text(resultf);
+event.preventDefault();
+});
+});
 
-});
-});
+
 //business Logic
 
 var divisible=function(answer){
   for(i=1; i<=answer; i++){
-    if(i%3===0){
-      console.log("ping");
-    }else if (i%5===0){
-      console.log("pong");
-    } 
+    if(i%15===0){
+      $("#output").append("<li>"+"pingpong"+"</li>");
+    }else if (i%3===0){
+      $("#output").append("<li>"+"ping"+"</li>");
+    } else if(i%5===0){
+      $("#output").append("<li>"+"pong"+"</li>");
+    }else{
+    
     }
   }
