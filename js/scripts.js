af01f0a1ca2e53341f85012db1dcef51bfeb5161
Brenda Kiptim <kiptim54@gmail.com>
2018-02-23 15:21:15 +0300

    add javascript else

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 $("#choice").click(function(){
 var answer=parseInt($("#number").val());
 var divisibility=divisible(answer);
-$("#list").text(divisibility);
+
 });
 });
 
 var divisible=function(answer){
   for(i=1; i<=answer; i++){
     if(i%3===0){
-      console.log("ping");
+      $("#list").innerHTML="ping"
     }else if (i%5===0){
       console.log("pong");
     } else if(i%15===0){
